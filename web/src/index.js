b6ff176e29ade414e89361659644f56f2632b26b
Edmund Mai <edmundmai@gmail.com>
2018-03-07 12:02:46 -0500

    view scaffold

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Homepage from "views/homepage";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Homepage />, document.getElementById("root"));
 registerServiceWorker();
