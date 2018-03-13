9adaba65fc440a01c9fc65eab6de3702140ab663
Edmund Mai <edmundmai@gmail.com>
2018-03-13 18:50:07 -0400

    less margin

--- src/views/homepage/slanted.js
+++ src/views/homepage/slanted.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 const Slanted = styled.div`
   min-height: ${props =>
     props.long ? "1000px" : props.medium ? "700px" : "550px"};
-  margin-top: ${props =>
-    props.long ? "-100px" : props.medium ? "-81px" : "-50px"};
+  margin-top: -50px;
   background: ${props => (props.colored ? "#EFF5F8" : "white")};
   background-image: url(${props => props.background});
   background-repeat: no-repeat;
